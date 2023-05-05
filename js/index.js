d09789edb501c8f4b684579213ab7be55f84b384
Bárbara Menares <barbara.menares@gmail.com>
2023-05-05 17:07:23 -0300

    Prueba JS primera versión

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/* prueba primer .js*/
+
+let apellido = prompt('Ingresa tu apellido');
+let nombre = prompt('Ingresa tu nombre');
+
+let nombrecompleto = nombre + " " + apellido;
+
+console.log(nombrecompleto);
+
+alert("¡Gracias! " + nombrecompleto + ", ahora vamos a hacer una suma");
+
+let num1 = Number(prompt('ingresa el primer número'));
+let num2 = Number(prompt('ingresa el segudo número'));
+
+let resultadosuma = num1 + num2;
+
+alert("El resultado es " + resultadosuma);
+console.log(resultadosuma);
